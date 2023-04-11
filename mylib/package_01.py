7ed0b643567c7bb77a4f6f071e787fc685dca60c
Oscar Mike <mike.claure@gmail.com>
2023-04-11 11:37:13 +0200

    add module 1

---
+++ mylib/package_01.py
@@ -0,0 +1,11 @@
+""" A module for greetings """
+
+
+def hello(name='Oscar Mike'):
+    """
+    Greetings function
+    :param name: name
+    :return: A greeting
+    """
+    message = f'Hello {name}, hope you are doing well!'
+    return message
