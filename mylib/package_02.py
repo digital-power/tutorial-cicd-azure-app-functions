73316aeb3fa5562aa9ed01c31225466f99b3fa7e
Oscar Mike <mike.claure@gmail.com>
2023-04-11 11:37:21 +0200

    add module 2

---
+++ mylib/package_02.py
@@ -0,0 +1,11 @@
+""" A module for goodbyes """
+
+
+def bye(name='Oscar Mike'):
+    """
+    A goodbye function
+    :param name: name
+    :return: a goodbye message
+    """
+    message = f'Bye {name}, hope to see you soon!'
+    return message
